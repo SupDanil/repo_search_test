94b37e2b076b29adc1b2436362786fea894616ca
Danil Kondratov <com.kondratov@gmail.com>
2022-02-08 07:49:12 +0300

    final

--- src/components/customButton.tsx
+++ src/components/customButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { buttonProps } from "../consts/interfaces";
 
 export const CustomButton = ({limit, fetchRepos} :buttonProps) => {
-    if (limit === 0)  return <div className="show_more_button_disabled">SHOW MORE</div>
+    if (limit === 0)  return <div className="show_more_button_disabled">SHOW MORE (5 results)</div>
     if (limit >= 25) return <div className="show_more_button_disabled">Количество запросов ограничено. Попробуйте позже!</div>;
     return <div className='show_more_button' onClick={()=>fetchRepos(true)}>SHOW MORE</div>;
 };
